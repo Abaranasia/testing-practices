2452a88bdd1f625adee10dacb9eb98be39c31ef0
Abaranasia <rankirlian@gmail.com>
2025-10-19 17:54:35 +0200

    feat: add UserForm component to add user data

---
+++ src/components/UserForm.tsx
@@ -0,0 +1,40 @@
+import { FormEvent, useState } from 'react'
+import { User } from '../App';
+
+  interface UserFormProps {
+    onUserAdd: (user: User) => void
+  }
+
+const UserForm = ({ onUserAdd }:  UserFormProps) => {
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onUserAdd({ name, email });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <button>Add User</button>
+    </form>
+  )
+}
+
+export default UserForm;
