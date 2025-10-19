088ef73db2b93e3ad0821fac13a14a5e3d24bd6a
Abaranasia <rankirlian@gmail.com>
2025-10-19 17:54:59 +0200

    feat: add UserList component to list user data

---
+++ src/components/UserList.tsx
@@ -0,0 +1,28 @@
+import { User } from "../App";
+
+const UserList = ({ users }: {users: User[]}) => {
+    const renderUsers = users.map((user) => {
+        return (
+            <tr key={user.name}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+            </tr>
+        )
+    });
+
+    return (
+        <table className="user-list">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderUsers}
+            </tbody>
+        </table>
+    )
+}
+
+export default UserList;
