bc148814277a0972b1517352883d462f4fe88c08
Abaranasia <rankirlian@gmail.com>
2025-10-26 16:10:29 +0100

    ref: add and improve tests for usersList component

--- src/__test__/UserList.test.tsx
+++ src/__test__/UserList.test.tsx
@@ -1,17 +1,42 @@
-
-
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import UserList from "../components/UserList.tsx";
 
-describe('UserList tests', () => {
-    it('shows a table', () => {
-        const mockUsers = [
-            { name: 'pepe', email: 'pp@pp.com' },
-            { name: 'paco', email: 'pc@pc.com' },
-        ];
+describe("UserList tests", () => {
+  it("render one row per user", () => {
+    renderComponent();
 
-        render(<UserList users={mockUsers} />)
-        const renderedTable = screen.getByRole('table');
-        expect(renderedTable).toBeInTheDocument();
-    });
+    const renderedTable = screen.getByRole("table");
+    expect(renderedTable).toBeInTheDocument();
+
+    // const rows = screen.getAllByRole('row');
+    // expect(rows).toHaveLength(3);
+
+    const rows = within(screen.getByTestId("users")).getAllByRole("row");
+    expect(rows).toHaveLength(2);
+  });
+
+  it("render the email and name of each user", () => {
+    const { users } = renderComponent();
+
+    for (let user of users) {
+      const name = screen.getByRole("cell", { name: user.name });
+      const email = screen.getByRole("cell", { name: user.email });
+
+      expect(name).toBeInTheDocument();
+      expect(email).toBeInTheDocument();
+    }
+  });
 });
+
+  function renderComponent() {
+    const users = [
+      { name: "pepe", email: "pp@pp.com" },
+      { name: "paco", email: "pc@pc.com" },
+    ];
+
+    render(<UserList users={users} />);
+
+    return {
+      users,
+    };
+  }
